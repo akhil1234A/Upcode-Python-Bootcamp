db2e84ba5cb0a69a5850520a39882ebd02d444ef
Akhil Anwer <40333606+akhil1234A@users.noreply.github.com>
2024-02-03 17:32:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+def load_products_data():
+  with open('products.json','r', encoding="UTF-8") as file:
+    return json.load(file)
+
+def save_products_data(products):
+  with open('products.json','w') as file:
+    json.dump(products,file,indent=4)
+@app.route('/',methods=['GET'])
+def hello():
+  return "hello"
+
+@app.route('/products',methods=['GET'])
+def get_products():
+  data = load_products_data()
+  return data
+
+@app.route('/products/<int:product_id>',methods=['GET'])
+def get_product_by_id(product_id):
+  products = load_products_data()
+  product = None
+  for p in products:
+    if p["id"] == product_id:
+      product = p
+      break
+  return jsonify(product) if product else ("Not found",404)
+    
+@app.route('/products', methods=['POST'])
+def create_product():
+  new_product = request.json
+  products = load_products_data()
+  products.append(new_product)
+  save_products_data(products)
+  return new_product
+
+@app.route('/products/<int:product_id>',methods=['PUT'])
+def update_product(product_id):
+  products = load_products_data()
+  product = None
+  for p in products:
+    if p["id"] == product_id:
+      product = p
+      break
+  update_product = request.json
+  product.update(update_product)
+  save_products_data(products)
+  return update_product
+
+@app.route('/products/<int:product_id>',methods=['DELETE'])
+def delete_product(product_id):
+  products = load_products_data()
+  
+  updated_list = list(filter(lambda p: p["id"] != product_id, products))
+  save_products_data(updated_list)
+  
+  return "Product Delted Successful", 204
+
+app.run(debug=True)
+
